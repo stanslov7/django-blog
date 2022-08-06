6ce7018fbe50a4d7209ce9df5d54a1f874772439
Stan Slov <s.slov7@gmail.com>
2022-08-06 03:32:46 -0400

    Added django_blog.css to new folder created in mysite/blogging/static added this css file to this static folder, then added to base.html file in mysite/mysite/templates where line at the very top was added for load static files and 2nd line in title tag added to reference this statis file through the link and href of the load static tag. Server was run and showed the GET static/django_blog.css with 200 success code was loaded. Next global nav and login additions.

--- blogging/views.py
+++ blogging/views.py
@@ -6,7 +6,7 @@ from blogging.models import Post
 
 
 # Create your views here.
-#=========================================================================
+#========================================================================
 #def stub_view(request, *args, **kwargs):
 #    body = "Stub View\n\n"
 #    if args:
@@ -16,7 +16,7 @@ from blogging.models import Post
 #        body += "Kwargs:\n"
 #        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
 #    return HttpResponse(body, content_type="text/plain")
-#=========================================================================
+#========================================================================
 
 # and add this view
 #=========================================================================
