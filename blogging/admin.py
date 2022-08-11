87e23017c054dad4792796c1f35860971b2b509c
Stan Slov <s.slov7@gmail.com>
2022-08-11 05:46:33 -0400

    Created classes for models: CategoryInline, PostAdmin, and CategoryAdmin. These models registered along with the previously defined original models had an inline of the Category.posts attr (via InlineModelAdmin object) added to the PostAdmin class, and with the posts field of Category's original model excluded within the CategoryAdmin class. These changes and registering the models respectively produced Assign 07's desired results of a Tabular selection of Category-posts relationships when Adding Post or editing an existing one, as well as removed the previously default many-to-many multi selection field from Adding Category or editing an existing, to where you may simply create Category with a description field (or edit) with the actual Category-posts relationships being added within the Add (or edit) Post page. Assign 7 ready to submit.

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,6 +3,36 @@ from django.contrib import admin
 from blogging.models import Post, Category  # <-- import Category
 
 # Register your models here.
+#admin.site.register(Post)  # and a new admin registration
+#admin.site.register(Category)               # <-- Register Category as well
 
-admin.site.register(Post)  # and a new admin registration
-admin.site.register(Category)               # <-- Register Category as well
+################################################
+# Need to Register the models? --> Did it as 
+# decorators + attached to relevant base model
+###############################################
+# admin.site.register(Post, PostAdmin)
+# admin.site.register(Category, CategoryAdmin)
+###############################################
+
+# an InlineModelAdmin to represent Categories on the Post admin view
+class CategoryInline(admin.TabularInline):  # or admin.StackedInline
+    model = Category.posts.through
+    # From the following line in models.py Category model class:
+    # posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # to represent Categories on the Post admin view
+    inlines = [
+        CategoryInline,
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # to exclude the ‘posts’ field from the form in your Category admin
+    exclude = ('posts',)
+    #exclude = [
+    #    'posts',
+    #]
